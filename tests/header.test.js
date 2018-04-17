e6410419d28b7950a3ae81fe04df68861e89bedf
Tony Shindo <honolulu3310@gmail.com>
2018-04-16 22:49:11 -0400

    Add browser test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,15 @@
-test('Add two numbers', () => {
+const puppeteer = require('puppeteer');
+
+test('Adds two numbers', () => {
   const sum = 1 + 2;
 
   expect(sum).toEqual(3);
+});
+
+test('We can launch a browser', async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+  await page.goto('localhost:3000');
 });
